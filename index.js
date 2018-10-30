58188d63870b1d3123e705857a57486319df17fa
ineffable-buffer-3376 <robertboze5@gmail.com>
2018-10-30 17:29:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ function nowServing(line) {
     return "Currently serving " + line.shift()+".";
   }
 }
+
+
+
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+  var lineNamesandNumbers = [];
+
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
+}
