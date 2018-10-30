018a86a44493219747464c522a025e904a514558
ineffable-buffer-3376 <robertboze5@gmail.com>
2018-10-30 16:46:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function takeANumber(noOfPeople,name){
 
 var pos = noOfPeople.length()-1;
 
-return "Welcome,"+ name +" ." + "You are number" +noOfPeople.length()-1+ "in line."
+return "Welcome,"+ name +" ." + "You are number" +noOfPeople.length -1+ "in line."
 
 
 
