964e9e9246331ec0d9e287cab649b77da1a8e3b1
ineffable-buffer-3376 <robertboze5@gmail.com>
2018-10-30 17:28:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function takeANumber(line, name){
 
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
+
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    //console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
