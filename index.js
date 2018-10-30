1d9c1357aa80a813703abf308f65af0d8dd776dd
ineffable-buffer-3376 <robertboze5@gmail.com>
2018-10-30 17:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function nowServing(line) {
     return "There is nobody waiting to be served!"
   } else {
     //console.log("Currently serving " + line.shift());
-    return "Currently serving " + line.shift();
+    return "Currently serving " + line.shift()+".";
   }
 }
